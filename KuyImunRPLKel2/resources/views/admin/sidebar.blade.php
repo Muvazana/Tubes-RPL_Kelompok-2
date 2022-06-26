b0961165cfd7be99dbd782e8b677fa40e6375166
Qomarudin Sifak <ragoistofer@gmail.com>
2022-06-26 22:40:16 +0700

    fix sidebar admin

--- KuyImunRPLKel2/resources/views/admin/sidebar.blade.php
+++ KuyImunRPLKel2/resources/views/admin/sidebar.blade.php
@@ -46,14 +46,6 @@
                     </svg>
                     Log
                 </a>
-
-                <a href="#" class="text-gray-500 hover:bg-blue-300 group flex items-center px-2 py-2 text-sm font-medium rounded-md">
-                    <!-- Heroicon name: outline/chart-bar -->
-                    <svg class="mr-3 flex-shrink-0 h-6 w-6 text-blue-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
-                    </svg>
-                    Reports
-                </a>
             </nav>
         </div>
         <div class="flex-shrink-0 flex border-t border-gray-200 p-4">
